bc8669423a61429cae1cc7bb7cb61cd451e331b8
Jared Brown <jaredj93@gmail.com>
2025-01-11 20:27:19 -0800

    fix: update debug logging

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -213,14 +213,14 @@ export class SmartRentWebsocketClient extends SmartRentApiClient {
   }
 
   private _handleWsError(error: WebSocket.ErrorEvent) {
-    this.log.error(`WebSocket error: ${JSON.stringify(error)}`);
+    this.log.error(`WebSocket error: ${error.message}`, error);
   }
 
   private _handleWsClose(event: WebSocket.CloseEvent) {
     this.log.debug(
       `WebSocket connection closed: Code: ${event.code}, Reason: ${
         event.reason
-      }, Event: ${JSON.stringify(event)}`,
+      }, Event: ${event}`,
       event
     );
     this.wsClient = this._initializeWsClient();
